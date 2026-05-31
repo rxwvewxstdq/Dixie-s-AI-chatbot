bfdee27dc4f6c815dca3d50455c4b88623a03b07
rxwvewxstdq <mtoropof@yandex.ru>
2026-05-31 19:34:33 +0300

    Delete app directory

--- app/login/page.tsx
+++
@@ -1,2 +0,0 @@
-import { AuthForm } from "@/components/AuthForm";
-export default function LoginPage() { return <div className="auth-wrap"><AuthForm mode="login" /></div>; }
--- app/register/page.tsx
+++
@@ -1,2 +0,0 @@
-import { AuthForm } from "@/components/AuthForm";
-export default function RegisterPage() { return <div className="auth-wrap"><AuthForm mode="register" /></div>; }
